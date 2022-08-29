579fa45149bf6de88d5479597083e6c0ac41a011
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2022-08-29 21:19:55 +0200

    WIP

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://http://lory1990.github.io/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
--- out/_next/static/SNBvKetG3rIHHN-NIwQP2/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fevents","\u002Fevents\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
