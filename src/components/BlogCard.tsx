6657e5b65197a408a70407d59c9a76c597b1d67a
DFMichelangelo <df.michelangelo@gmail.com>
2022-08-29 23:52:22 +0200

    ✨ BlogCard Done

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Link, Theme, Typography, useTheme } from "@mui/material"
+import { Button, Card, Link, Slide, Typography, useTheme } from "@mui/material"
 import { Box } from "@mui/system"
 import hexRgb from "hex-rgb"
 import useHover from "../hooks/useHover"
@@ -12,80 +12,93 @@ interface BlogCardProps {
 export default function BlogCard({ image, title, description, link }: BlogCardProps) {
     const theme = useTheme()
     const { ref: refButtonHover, isHovered: isButtonHovered } = useHover()
+    const { ref: refCardHover, isHovered: isCardHovered } = useHover()
     const primaryColorRGB = hexRgb(theme.palette.primary.main)
+
+
 
     return (
         <Card
+            ref={refCardHover}
             sx={{
                 width: "300px",
                 height: "400px",
-
+                position: "relative",
+                backgroundImage: `url(${image})`,
+                backgroundSize: "100%",
+                backgroundPosition: "center",
+                transition: "background-size 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+                WebkitTransition: "background-size 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+                ...isCardHovered && {
+                    backgroundSize: "130%"
+                }
             }}>
+
+
             <Box
                 sx={{
-                    backgroundPosition: "center",
-                    backgroundSize: "100%",
-                    backgroundImage: `url(${image})`,
-                    transition: "background-size 2s ease-in",
+                    position: "absolute",
                     height: "100%",
-                    WebkitTransition: "background-size 2s ease-in",
-                    ":hover": {
-                        backgroundSize: "130%"
+                    width: "100%",
+                    opacity: 1,
+                    background: "linear-gradient(0deg, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.25) 40%)",
+                    WebkitTransition: "opacity 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+                    transition: "opacity 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+                    ...isCardHovered && {
+                        opacity: 0
+
                     }
-                }}>
+                }}
+            />
+            <Box
+                sx={{
+                    position: "absolute",
+                    WebkitTransition: "opacity 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+                    transition: "opacity 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+                    height: "100%",
+                    width: "100%",
+                    background:
+                        `linear-gradient(0deg, rgba(0,0,0,0.7) 0%,
+                    rgba(
+                    ${primaryColorRGB.red / 2},
+                    ${primaryColorRGB.green / 2},
+                    ${primaryColorRGB.blue},0.3) 50%,
+                    rgba(
+                    ${primaryColorRGB.red},
+                    ${primaryColorRGB.green},
+                    ${primaryColorRGB.blue},0.6) 100%)`,
+                    opacity: 0,
+                    ...isCardHovered && {
+                        opacity: 1
+                    }
+                }} />
 
-                <Box
-                    sx={theme => ({
-                        background: "linear-gradient(0deg, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.25) 40%)",
-                        height: "100%",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "end",
 
-                        ":hover": {
-
-                            background:
-                                `linear-gradient(0deg, rgba(0,0,0,0.7) 0%,
-                            rgba(
-                            ${primaryColorRGB.red / 2},
-                            ${primaryColorRGB.green / 2},
-                            ${primaryColorRGB.blue},0.7) 50%,
-                            rgba(
-                            ${primaryColorRGB.red},
-                            ${primaryColorRGB.green},
-                            ${primaryColorRGB.blue},0.8) 100%)`
-                        }
-                    })}
-
-                >
-                    <Box sx={{ margin: "30px" }}>
-                        <Typography
-                            sx={{
-                                fontSize: "24px",
-                                fontWeight: 600,
-                                color: "white"
-                            }}>
-                            {title}
-                        </Typography>
-                        <Typography
-                            variant="body1"
-                            sx={{
-                                color: "white"
-                            }}>
-                            {description}
-                        </Typography>
+            <Box sx={{ position: "absolute", bottom: "10px" }}>
+                <Box sx={{ margin: "30px" }}>
+                    <Typography
+                        sx={{
+                            fontSize: "24px",
+                            fontWeight: 600,
+                            color: "white"
+                        }}>
+                        {title}
+                    </Typography>
+                    <Typography variant="body1" sx={{ color: "white" }}>
+                        {description}
+                    </Typography>
+                    <Slide direction="up" in={isCardHovered}>
                         <Button
-
                             component={Link}
                             href={link}
                             ref={refButtonHover}
                             variant={isButtonHovered ? "contained" : "outlined"}
                             color="primary"
-                            sx={{ marginTop: "30px", color: !isButtonHovered && "white", border: `2px solid ${theme.palette.primary}` }}
+                            sx={{ marginTop: "30px", color: !isButtonHovered && "white", border: `2px solid ${theme.palette.primary}`, pointerEvents: "auto" }}
                         >
                             Read More
                         </Button>
-                    </Box>
+                    </Slide>
                 </Box>
             </Box>
         </Card >
