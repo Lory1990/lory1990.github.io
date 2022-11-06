0259d644f360d851fe60bdfc766f6b143919dbda
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2022-11-06 17:53:08 +0100

    build fix

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: false,
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
+  images: {
+    unoptimized: true,
   },
 }
 
