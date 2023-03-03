f35c7e2fa25765fbbd5a382a0e4edcebe8568a75
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2023-03-03 10:05:05 +0100

    added ga

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,24 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 //GA SCRIPT FROM - https://nextjs.org/docs/messages/next-script-for-ga
 export default function Document() {
+
+  console.log("process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID)
   return (
     <Html>
       <Head />
       <body>
-        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
-          <>
-            <Script async src={"https://www.googletagmanager.com/gtag/js?id=" + process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} strategy="afterInteractive"></Script>
-            <Script id="google-analytics-script" strategy="afterInteractive">
-              {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-            `}
-            </Script>
-          </>
-        )}
+        <Script async src={"https://www.googletagmanager.com/gtag/js?id=" + process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} strategy="afterInteractive"></Script>
+        <Script id="google-analytics-script" strategy="afterInteractive">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}
+        </Script>
         <Main />
         <NextScript />
       </body>
