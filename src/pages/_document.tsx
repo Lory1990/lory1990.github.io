dc48d4bd79f732159d212def6d5b899dbff71c2b
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2023-05-13 12:26:18 +0200

    aded meta tag google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,9 @@ export default function Document() {
   console.log("process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID)
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="nyek_hMs6Apq-qxWkTCgTjKKrGCFW8WvghJFdoa5L74" />
+      </Head>
       <body>
         <Script async src={"https://www.googletagmanager.com/gtag/js?id=" + process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} strategy="afterInteractive"></Script>
         <Script id="google-analytics-script" strategy="afterInteractive">
