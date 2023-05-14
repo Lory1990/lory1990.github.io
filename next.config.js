8f171d973a37d5db79349f6bf75da5cee8678c6d
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2023-05-15 00:33:11 +0200

    added redirects

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: false,
   images: {
     unoptimized: true
-  }
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/portfolio/:slug',
+        destination: '/projects/:slug',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
