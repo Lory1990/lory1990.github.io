19de9626de4efdec62c35b9068613dd4a154b1f6
Lory1990 <Lory1990@users.noreply.github.com>
2023-08-07 17:24:37 +0000

    Fix google maps

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -5,7 +5,7 @@ const GoogleMap: React.FC<IGoogleMapProps> = ({}) => {
   const ref = useRef()
   const milan = { lat: 45.4641013, lng: 9.1897325 }
   useEffect(() => {
-    const googleMap = new window.google.maps.Map(ref.current, {
+    new window.google.maps.Map(ref.current, {
       streetViewControl: false,
       zoomControl: false,
       fullscreenControl: false,
@@ -13,11 +13,6 @@ const GoogleMap: React.FC<IGoogleMapProps> = ({}) => {
       zoom: 12,
       center: milan
     })
-
-    // const marker = new google.maps.Marker({
-    //   position: milan,
-    //   map: googleMap
-    // });
   }, [])
 
   return <div style={{ width: "100%", height: "100%" }} ref={ref} id="map" />
