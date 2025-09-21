14b85ec91d79207fe301ae00f92d29662b1044e5
Lorenzo De Francesco <Lory1990@users.noreply.github.com>
2025-09-21 12:12:13 +0200

    Added dev fest venezia 2024

--- src/assets/events-list.ts
+++ src/assets/events-list.ts
@@ -24,6 +24,17 @@ export interface IEvent {
 
 const events: IEvent[] = [
   {
+    "title": "Anche le infrastrutture cloud possono essere scriptate",
+    slug: "anche-le-infrastrutture-cloud-possono-essere-scriptate",
+    link:"https://devfest24.gdgvenezia.it/",
+    isOnline: false,
+    venue: "Campus Scientifico Università Ca' Foscari",
+    date: "2024-11-16",
+    image: "/img/2024-dev-fest-venezia.png",
+    description: `Sono finiti i tempi in cui bastava un database e un server PHP per avere un applicazione funzionante; adesso la situazione è molto più complessa, le componenti sono tante e devono essere correttamente create ed orchestrate. Possiamo farlo a mano, ma perché non creare uno script che si occupi di creare e tenere questa infrastruttura? Così sarà facile tenere traccia nel tempo delle modifiche e casomai migrare da una parte all'altra. Ecco terraform fa esattamente questo e in questo talk vedremo come si può usare in un ambiente locale per orchestrare container ed in uno cloud per creare tutte le risorse di cui abbiamo bisogno. Se anche tu non vuoi più passare le ore a creare le infrastrutture cloud non puoi perderti questo talk!`,
+    shortDescription: "Terraform aplpicato ad azure"
+  },
+  {
     title: "IT Governance con Lorenzo De Francesco",
     slug: "it-governance-con-lorenzo-de-francesco",
     link: "https://open.spotify.com/episode/2ushjt8QXbhXiYNplzxEeH",
